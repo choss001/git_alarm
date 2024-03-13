ad4ccff839de5213693f9682508e190e1200d467
choss001 <choss01@naver.com>
2024-03-13 14:47:17 +0900

    initial project

---
+++ demo/src/main/java/co/kr/GitGrassApplication.java
@@ -0,0 +1,13 @@
+package co.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitGrassApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/co/kr/GitGrassApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
